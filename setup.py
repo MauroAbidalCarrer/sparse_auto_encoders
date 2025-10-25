244bd08dc4e9269ab897c133bae7d759e81ed684
Mauro Abidal Carrer <mauroabida@yahoo.fr>
2025-10-25 15:01:51 +0200

    Clean: Removed useless lines in setup.py.

--- setup.py
+++ setup.py
@@ -5,8 +5,5 @@ setup(
     version='0.1',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
-    install_requires=[
-        # List your dependencies here, e.g.,
-        # 'requests>=2.20.0',
-    ],
+    install_requires=[],
 )
