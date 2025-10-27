5c93a435cf4df32613eba717fd8e0323b43e081c
Mauro Abidal Carrer <mauroabidal@yahoo.fr>
2025-10-27 18:44:10 +0000

    Feat: Added wandb logging.

--- src/SAE_training.py
+++ src/SAE_training.py
@@ -3,6 +3,7 @@ from tqdm import tqdm
 from functools import partial
 from contextlib import nullcontext
 
+import wandb
 import torch
 import numpy as np
 import pandas as pd
@@ -11,23 +12,39 @@ from torch import Tensor
 from torch.utils.data import DataLoader, TensorDataset, random_split
 
 
+# Torch setup
+torch.set_float32_matmul_precision('high')
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+def can_use_bfloat16(device: torch.device) -> bool:
+    if not torch.cuda.is_available():
+        return False
+    try:
+        # get capability for current device index
+        major, minor = torch.cuda.get_device_capability(device.index)
+    except Exception:
+        major, minor = torch.cuda.get_device_capability()
+    # Ampere (sm_80) and newer support efficient bfloat16
+    return major >= 8
+
+USE_BFLOAT16 = can_use_bfloat16(device)
+print("USE_BFLOAT16 autocast:", USE_BFLOAT16)
+# autocast context factory (nullcontext if not available)
+if USE_BFLOAT16:
+    autocast_ctx = partial(torch.autocast, device_type=device.type, dtype=torch.bfloat16)
+else:
+    autocast_ctx = nullcontext
+
 # -------- USER: paths to your data files --------
 ACTIVATIONS_PATH = "dataset/middle_layer_activations.pt"  # shape: (N, seq_len, hidden_dim)
 OUT_DIR = "sae_output"
 os.makedirs(OUT_DIR, exist_ok=True)
 
 # -------- HYPERPARAMS --------
-POOL_METHOD = "last"      # options: "mean", "last", "max"
-BOTTLE_DIM = 128
-HIDDEN_FACTOR = 0.5       # hidden_dim * factor -> first encoder hidden
 BATCH_SIZE = 256
 LR = 1e-3
 WEIGHT_DECAY = 1e-5
-EPOCHS = 6
+SAE_EPOCHS = 6
 LAMBDA_L1 = 1e-4          # coefficient for L1 on latent
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print("batch size:", BATCH_SIZE)
-
 # -------- Utility: load activations and labels --------
 activations: torch.Tensor = torch.load(ACTIVATIONS_PATH, weights_only=True)  # (N_TOKENS, EMBED_DIMS)
 activations = activations.float()
@@ -49,14 +66,14 @@ val_loader = DataLoader(val_ds, batch_size=BATCH_SIZE)
 class SparseAutoEncoder(nn.Module):
     def __init__(self, model_embed_size: int, sparse_activation_expantion: int, dropout_ratio: float):
         super().__init__()
-        sparse_activations_size = model_embed_size * sparse_activation_expantion
-        print("sparse_activations_size", sparse_activations_size)
+        self.sparse_activations_size = model_embed_size * sparse_activation_expantion
+        print("sparse_activations_size", self.sparse_activations_size)
         self.encoder = nn.Sequential(
-            nn.Linear(model_embed_size, sparse_activations_size),
+            nn.Linear(model_embed_size, self.sparse_activations_size),
             nn.ReLU(),
         )
         self.decoder = nn.Sequential(
-            nn.Linear(sparse_activations_size, model_embed_size),
+            nn.Linear(self.sparse_activations_size, model_embed_size),
         )
 
     def forward(self, x):
@@ -74,68 +91,73 @@ sae = sae.to(device)
 optimizer = torch.optim.AdamW(sae.parameters(), lr=LR, weight_decay=WEIGHT_DECAY)
 mse_loss = nn.MSELoss(reduction="mean")
 
-
-torch.set_float32_matmul_precision('high')
-def can_use_bfloat16(device: torch.device) -> bool:
-    if not torch.cuda.is_available():
-        return False
-    try:
-        # get capability for current device index
-        major, minor = torch.cuda.get_device_capability(device.index)
-    except Exception:
-        major, minor = torch.cuda.get_device_capability()
-    # Ampere (sm_80) and newer support efficient bfloat16
-    return major >= 8
-
-USE_BFLOAT16 = can_use_bfloat16(device)
-print("USE_BFLOAT16 autocast:", USE_BFLOAT16)
-# autocast context factory (nullcontext if not available)
-if USE_BFLOAT16:
-    autocast_ctx = partial(torch.autocast, device_type=device.type, dtype=torch.bfloat16)
-else:
-    autocast_ctx = nullcontext
+# Weights and Biases
+LOG_COMMIT_FREQ = 100
+wandb.init(
+    project="gpt-training",
+    config={
+        "weight_decay": WEIGHT_DECAY,
+        "lr": LR,
+        "BATCH_SIZE": BATCH_SIZE,
+        "l1_loss_weight": LAMBDA_L1,
+        "sae_training_epochs": SAE_EPOCHS,
+        "using_bfloat16": USE_BFLOAT16,
+        "sparse_activations_size": sae.sparse_activations_size,
+    }
+)
 
 def train_sae(sae: nn.Module):
-    eval_model(sae, 0, 0)
-    for epoch in range(1, EPOCHS + 1):
+    step = 0
+    eval_model(sae, step)
+    for _ in range(1, SAE_EPOCHS + 1):
         sae.train()
-        train_loss = 0.0
         for (xb, ) in tqdm(train_loader):
             xb = xb.to(device)
             with autocast_ctx():
                 reconstruction, sparse_activations = sae(xb)
-                loss_recon = mse_loss(reconstruction, xb)
-                loss_l1 = sparse_activations.abs().mean()
-                loss = loss_recon + LAMBDA_L1 * loss_l1
+                reconstruction_loss = mse_loss(reconstruction, xb)
+                l0_loss = (sparse_activations > 0).float().mean()
+                l1_loss = sparse_activations.abs().mean()
+                loss = reconstruction_loss + LAMBDA_L1 * l1_loss
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            train_loss += loss.item() * xb.size(0)
+            wandb.log(
+                data={
+                    "training/recon_loss": reconstruction_loss.item(),
+                    "training/l0_loss": l0_loss.item(),
+                    "training/loss_l1": l1_loss.item(),
+                    "training/loss": loss.item(),
+                },
+                step=step,
+                commit=step % LOG_COMMIT_FREQ == 0,
+            )
+            step += 1
+        eval_model(sae, step)
 
-        train_loss /= len(train_loader.dataset)
-        eval_model(sae, epoch, train_loss)
-
-def eval_model(model: nn.Module, epoch: int, train_loss: float):
+def eval_model(model: nn.Module, step: int):
     model.eval()
-    val_loss = 0
-    l0_norm_loss = 0
-    zs_val = []
+    l0_loss = 0
+    l1_loss = 0 
+    reconstruction_loss = 0
     with torch.no_grad():
         for (x, ) in val_loader:
             x = x.to(device)
+            batch_weight = x.shape[0] / len(val_loader.dataset)
             reconstruction, sparse_activations = model(x)
-            loss_recon = mse_loss(reconstruction, x)
-            loss_l1 = sparse_activations.abs().mean()
-            loss = loss_recon + LAMBDA_L1 * loss_l1
-            val_loss += loss.item() * x.size(0)
-            zs_val.append(sparse_activations.cpu().numpy())
-            batch_l0_norm_loss = (sparse_activations != 0).float().mean().item()
-            l0_norm_loss = batch_l0_norm_loss * x.shape[0]
-
-    val_loss /= len(val_loader.dataset)
-    zs_val = np.vstack(zs_val) if len(zs_val) else np.zeros((0, BOTTLE_DIM))
-    print(f"Epoch {epoch} | Train loss {train_loss:.3f} | Val loss {val_loss:.3f} | l0_norm_loss {l0_norm_loss:.3f}")
-    classify_category_from_sae_features(model)
+            l1_loss += sparse_activations.abs().mean().item() * batch_weight
+            l0_loss += (sparse_activations > 0).float().mean().item() * batch_weight
+            reconstruction_loss += mse_loss(reconstruction, x).item() * batch_weight
+    wandb.log(
+        data={
+            "validation/l1_loss": l1_loss,
+            "validation/l0_loss": l0_loss,
+            "validation/reconstruction_loss": reconstruction_loss,
+            "validation/loss": reconstruction_loss + l1_loss * LAMBDA_L1
+        },
+        step=step
+    )
+    classify_category_from_sae_features(model, step)
     
 class TokenQuestionClassifier(nn.Module):
     def __init__(self, sae: nn.Module, n_targets: int):
@@ -152,7 +174,7 @@ class TokenQuestionClassifier(nn.Module):
         _, sae_features = self.sae(activations)
         return self.clssifier(sae_features)
     
-def classify_category_from_sae_features(sae: nn.Module):
+def classify_category_from_sae_features(sae: nn.Module, step:int):
     set_require_grad(sae, False)
     cls_dataset = mk_token_category_dataset()
     n_targets = cls_dataset.tensors[1].shape[1]
@@ -206,6 +228,15 @@ def classify_category_from_sae_features(sae: nn.Module):
                 )
                 val_accuracy += batch_accuracy * x.size(0) / len(train_loader.dataset)
         print(f"epoch {epoch}, train loss {train_loss:.3f}, train_accuracy {train_accuracy:.3f}, val loss {val_loss:.3f}, val accuracy {val_accuracy:.3f}")
+    wandb.log(
+        data={
+            "calssification/train_loss": train_loss,
+            "calssification/train_accuracy": train_accuracy,
+            "calssification/validation_loss": val_loss,
+            "calssification/validation_accuracy": val_accuracy,
+        },
+        step=step,
+    )
     set_require_grad(sae, True)
 
 def set_require_grad(module: nn.Module, value: bool):
